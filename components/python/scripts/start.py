01f1a2ffd0558aeaf449de006fdad69f3dd9e5e6
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2016-05-11 12:28:08 +0200

    python runtime checks

---
+++ components/python/scripts/start.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from os.path import join, dirname
+
+from cloudify import ctx
+
+ctx.download_resource(
+    join('components', 'utils.py'),
+    join(dirname(__file__), 'utils.py'))
+import utils  # NOQA
+
+
+pip_result = utils.sudo(['pip'], ignore_failures=True)
+if pip_result.returncode == 0:
+    ctx.logger.info('pip was installed correctly')
+else:
+    ctx.logger.error('pip was not installed')
