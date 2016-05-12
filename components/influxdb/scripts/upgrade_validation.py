f6801f68f04f57a8537894909cdf2e8f995b8301
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2016-05-12 14:58:49 +0200

    influxdb validation

---
+++ components/influxdb/scripts/upgrade_validation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os.path import join, dirname
+from cloudify import ctx
+
+ctx.download_resource(
+    join('components', 'utils.py'),
+    join(dirname(__file__), 'utils.py'))
+
+import utils  # NOQA
+
+INFLUX_SERVICE_NAME = 'influxdb'
+
+utils.upgrade_validation_directories(INFLUX_SERVICE_NAME)
+
+if not utils.systemd.is_alive(INFLUX_SERVICE_NAME):
+    raise RuntimeError('InfluxDB service must be runnint to allow data '
+                       'migration')
